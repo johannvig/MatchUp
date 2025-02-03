1d6000467dc5a5000c23f09bcce1839bcdaa44b4
maelg <maelgarnier5@gmail.com>
2025-02-03 11:57:01 -0500

    ajout d'autres fonctions de connexion

--- frontend/src/app/controleurs/connexionControleur.ts
+++ frontend/src/app/controleurs/connexionControleur.ts
@@ -60,4 +60,83 @@ export async function connexion(email: string, motDePasse: string) : Promise<boo
     }
 }
 
+/**
+ * Fonction permettant d'enregistrer un nouvel utilisateur dans la base de données
+ * le token de l'utilisateur est automatiquement enregistré dans les cookies.
+ * @param prenom
+ * @param nom
+ * @param sexe
+ * @param email
+ * @param nationalite
+ * @param dateNaissance
+ * @param motDePasse
+ * @returns true si l'enregistrement a réussi, false sinon
+ */
+export async function enregistrer(
+    prenom : string,
+    nom : string,
+    sexe : string,
+    email: string,
+    nationalite: string,
+    dateNaissance: string,
+    motDePasse: string
+) {
 
+    //envoi de la requete
+    const res = await fetch(`${process.env}/enregistrer`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({prenom, nom, sexe, email, nationalite, dateNaissance, motDePasse}),
+    });
+
+    //Traitement de la réponse
+    if (res.status === 200) {
+        const data = await res.json();
+        const token : string = data.token;
+        await sauverToken(token)
+        //TODO: Récuperer aussi les données du compte de l'utilisateur pour les récuperer
+        return true;
+    } else {
+        //TODO: Retourner un code d'erreur, si possible avec une enumeration (plus simple)
+        return false;
+    }
+}
+
+/**
+ * Fonction permettant de vérifier si le token de connexion enregistré dans les cookies est valide
+ * Pas besoin de passer le token en paramètre, il est automatiquement récupéré dans les cookies
+ * @return true si le token est valide, false sinon
+ */
+export async function verifierToken() {
+
+    //récuperation du token
+    const token = await recupererToken();
+    //si le token est null, on retourne false directement
+    if(token == null) {
+        return false;
+    }
+
+    //Envoi de la requête de connexion à l'API
+    const res = await fetch(`${process.env}/verifierToken`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({token}),
+    });
+
+    //Traitement de la réponse
+    if (res.status === 200) {
+        const data = await res.json();
+        const token : string = data.token;
+        //On sauve le nouveau token retourné par l'API
+        await sauverToken(token)
+        //TODO: Récuperer aussi les données du compte de l'utilisateur pour les récuperer
+        return true;
+    } else {
+        //TODO: Retourner un code d'erreur, si possible avec une enumeration (plus simple)
+        return false;
+    }
+}
