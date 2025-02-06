33673ca58c6f9139aa9c5e41eb41b2c4fde73823
ewen <66205491+ewenman5137@users.noreply.github.com>
2025-02-06 13:39:26 -0500

    udpate

--- frontend/src/app/Composants/PanelAdmin/page.tsx
+++ frontend/src/app/Composants/PanelAdmin/page.tsx
@@ -1,48 +1,56 @@
-import './style.css'
+"use client"; // Indique que ce fichier doit être exécuté côté client
+
+import { usePathname } from "next/navigation";
+import "./style.css";
+
 export default function Header() {
+    const pathname = usePathname(); // Récupère l'URL actuelle
+
     return (
-      <div id="panel">
-        <button id='btnRetour'><img src="PanelAdmin/retour.svg" alt="" /></button>
-        <img id="logo" src="PanelAdmin/logoUqac.png" alt="" />
-        <div id="containeurRedirection">
-            <a className="redirection">
-                <img src="PanelAdmin/dashboard.svg" alt="" />
-                <p>Dashboard</p>
-                <img src="PanelAdmin/selection.svg" alt="" />
-            </a>
-            <a className="redirection">
-                <img src="PanelAdmin/compte.svg" alt="" />
-                <p>Utilisateur</p>
-                <img src="PanelAdmin/selection.svg" alt="" />
-            </a>
-            <a className="redirectionEnCours">
-                <img src="PanelAdmin/agenda.svg" alt="" />
-                <p>Agenda</p>
-                <img src="PanelAdmin/selection.svg" alt="" />
-            </a>
-            <a className="redirection">
-                <img src="PanelAdmin/comptabilite.svg" alt="" />
-                <p>Comptabilité</p>
-                <img src="PanelAdmin/selection.svg" alt="" />
-            </a>
-        </div>
-        <div id="footer">
-            <div id="containeurInfoAdmin">
-                <img src="PanelAdmin/compte.svg" alt="" />
-                <div id="nomFonction">
-                    <p className="nom">Jean-eude</p>
-                    <p>Admin</p>
+        <div id="panel">
+            <button id='btnRetour'>
+                <img src="../PanelAdmin/retour.svg" alt="" />
+            </button>
+            <img id="logo" src="../PanelAdmin/logoUqac.png" alt="" />
+            <div id="containeurRedirection">
+                <a className={pathname === "/admin/dashboard" ? "redirectionEnCours" : "redirection"} href="/admin/dashboard">
+                    <img src="../PanelAdmin/dashboard.svg" alt="" />
+                    <p>Dashboard</p>
+                    <img src="../PanelAdmin/selection.svg" alt="" />
+                </a>
+                <a className={pathname === "/admin/utilisateur" ? "redirectionEnCours" : "redirection"} href="/admin/utilisateur">
+                    <img src="../PanelAdmin/compte.svg" alt="" />
+                    <p>Utilisateur</p>
+                    <img src="../PanelAdmin/selection.svg" alt="" />
+                </a>
+                <a className={pathname === "/admin/agenda" ? "redirectionEnCours" : "redirection"} href="/admin/agenda">
+                    <img src="../PanelAdmin/agenda.svg" alt="" />
+                    <p>Agenda</p>
+                    <img src="../PanelAdmin/selection.svg" alt="" />
+                </a>
+                <a className={pathname === "/admin/comptabilite" ? "redirectionEnCours" : "redirection"} href="/admin/comptabilite">
+                    <img src="../PanelAdmin/comptabilite.svg" alt="" />
+                    <p>Comptabilité</p>
+                    <img src="../PanelAdmin/selection.svg" alt="" />
+                </a>
+            </div>
+            <div id="footer">
+                <div id="containeurInfoAdmin">
+                    <img id="photoDeProfil" src="../PanelAdmin/compte.svg" alt="" />
+                    <div id="nomFonction">
+                        <p className="nom">Jean-eude</p>
+                        <p>Admin</p>
+                    </div>
+                </div>
+                <div className="actionFooter">
+                    <img src="../PanelAdmin/deconnexion.svg" alt="" />
+                    <h2>Deconnexion</h2>
+                </div>
+                <div id="suppression">
+                    <img src="../PanelAdmin/croix.svg" alt="" />
+                    <h2>Supprimer le compte</h2>
                 </div>
             </div>
-            <div className="actionFooter">
-                <img src="PanelAdmin/deconnexion.svg" alt="" />
-                <h2>Deconnexion</h2>
-            </div>
-            <div className="actionFooter">
-                <img src="PanelAdmin/croix.svg" alt="" />
-                <h2>Supprimer le compte</h2>
-            </div>
         </div>
-      </div>
     );
 }
---
+++ frontend/src/app/admin/agenda/page.tsx
@@ -0,0 +1,8 @@
+
+import Panel from '../../Composants/PanelAdmin/page'; 
+
+export default function Admin() {
+    return (
+      <Panel/>
+    );
+}
